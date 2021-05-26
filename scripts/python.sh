eb948a877447c308ce30eb9db015133e71e759e6
devhijazi <hijazi.root@gmail.com>
2021-05-25 21:57:51 -0400

    add script to download python

---
+++ scripts/python.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+clear
+echo "Instalando Python3..."
+sudo apt install python3;
+clear 
+
+echo "Instalando a lib python-pip..."
+sudo apt install python-pip;
+clear
+
+echo "Instalando wine..." 
+sudo apt install wine;
+clear
+
+echo "Instalando Docker..."
+sudo apt install docker;
+clear
+
+echo "Instalando Docker Compose..."
+sudo apt install docker-compose;
+clear
+
+echo "Instalando o Git"
+sudo apt install git
