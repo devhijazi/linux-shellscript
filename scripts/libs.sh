2d2e8590282fc24b100ac9e30216f3d63b87ff7b
devhijazi <hijazi.root@gmail.com>
2021-05-25 21:57:43 -0400

    add script to download libs

---
+++ scripts/libs.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+clear
+echo "Instalando wine..." 
+sudo apt install wine;
+clear
+
+echo "Instalando Docker..."
+sudo apt install docker;
+clear
+
+echo "Instalando Docker Compose..."
+sudo apt install docker-compose;
+clear
+
+echo "Instalando o Git..."
+sudo apt install git
+clear
+
+echo "Instalando o WGET..."
+sudo apt install wget
+
