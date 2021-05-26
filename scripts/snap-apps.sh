9ebf3a259b5545bec25bd09e1a08ad4480ed2f39
devhijazi <hijazi.root@gmail.com>
2021-05-25 21:57:59 -0400

    add script to download snap apps

---
+++ scripts/snap-apps.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+clear
+echo "Baixando o Spotify usando o SNAP..."
+sudo snap install spotify;
+clear
+
+echo "Baixando o INSOMNIA via SNAP..."
+sudo snap install insomnia;
+
+
+#---------------------LISTA DE PROGRAMAS-------------------#
+# sudo snap install slack --classic &&  
+# sudo snap install code --classic &&  
+# sudo snap install --edge node --classic && 
+# sudo snap install insomnia &&  
+# sudo snap install spotify &&
+# sudo snap install wps-office-multilang && 
+#---------------------LISTA DE PROGRAMAS-------------------#
